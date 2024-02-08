1c512b5715ca8c438d57d247bb1473ee5b0960e5
Tobias Lindberg <uet.lindberg@outlook.com>
2024-02-08 13:23:07 +0100

    -

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -16,17 +16,85 @@ let scoreCount = 0;
 
 console.log(questionsList);
 
-for (const q of questions) {
-  console.log(q);
+displayQuestions();
 
-  const li = document.createElement("li");
-  const card = document.createElement("div");
-  const cardBody = document.createElement("div");
-  const cardTitle = document.createElement("h5");
-  const cardFooter = document.createElement("div");
-  const trueBtn = document.createElement("button");
-  const falseBtn = document.createElement("button");
+function displayQuestions() {
+  for (const q of questions) {
+    console.log(q);
 
+    const li = document.createElement("li");
+    const card = document.createElement("div");
+    const cardBody = document.createElement("div");
+    const cardTitle = document.createElement("h5");
+    const cardFooter = document.createElement("div");
+    const trueBtn = document.createElement("button");
+    const falseBtn = document.createElement("button");
+
+    applyStyles(li, card, cardBody, cardTitle, cardFooter, trueBtn, falseBtn);
+
+    trueBtn.innerText = "True";
+    trueBtn.onclick = () => {
+      answerBtnClick(cardTitle, cardBody, trueBtn, falseBtn, q, true);
+    };
+
+    falseBtn.innerText = "False";
+    falseBtn.onclick = () => {
+      answerBtnClick(cardTitle, cardBody, trueBtn, falseBtn, q, false);
+    };
+
+    cardTitle.innerText = q.statement;
+
+    cardBody.appendChild(cardTitle);
+    cardFooter.appendChild(trueBtn);
+    cardFooter.appendChild(falseBtn);
+
+    card.appendChild(cardBody);
+    card.appendChild(cardFooter);
+    li.appendChild(card);
+
+    questionsList.appendChild(li);
+  }
+}
+
+function answerBtnClick(
+  cardTitle,
+  cardBody,
+  trueBtn,
+  falseBtn,
+  question,
+  answer
+) {
+  if (question.correctAnswer === answer) {
+    {
+      console.log("Correct answer");
+      scoreCount++;
+      score.innerText = scoreCount;
+
+      cardBody.classList.add("bg-success");
+      cardTitle.classList.remove("text-info");
+      cardTitle.classList.add("text-white");
+    }
+  } else {
+    {
+      console.log("Incorrect answer");
+      cardBody.classList.add("bg-danger");
+      cardTitle.classList.add("text-white");
+      cardTitle.classList.remove("text-info");
+    }
+  }
+  trueBtn.disabled = true;
+  falseBtn.disabled = true;
+}
+
+function applyStyles(
+  li,
+  card,
+  cardBody,
+  cardTitle,
+  cardFooter,
+  trueBtn,
+  falseBtn
+) {
   li.classList.add("list-group-item", "bg-dark", "border-danger");
   card.classList.add("card", "bg-dark", "container");
   cardBody.classList.add("card-body", "row");
@@ -39,64 +107,4 @@ for (const q of questions) {
   cardTitle.classList.add("card-title", "text-info", "text-center");
   trueBtn.classList.add("btn", "btn-success", "mx-1", "col-2");
   falseBtn.classList.add("btn", "btn-danger", "mx-1", "col-2");
-
-  trueBtn.innerText = "True";
-  trueBtn.onclick = () => {
-    if (q.correctAnswer === true) {
-      {
-        console.log("Correct answer");
-        scoreCount++;
-        score.innerText = scoreCount;
-
-        cardBody.classList.add("bg-success");
-        cardTitle.classList.remove("text-info");
-        cardTitle.classList.add("text-white");
-      }
-    } else {
-      {
-        console.log("Incorrect answer");
-        cardBody.classList.add("bg-danger");
-        cardTitle.classList.remove("text-info");
-        cardTitle.classList.add("text-white");
-      }
-    }
-    trueBtn.disabled = true;
-    falseBtn.disabled = true;
-  };
-
-  falseBtn.innerText = "False";
-  falseBtn.onclick = () => {
-    if (q.correctAnswer === false) {
-      {
-        console.log("Correct answer");
-        scoreCount++;
-        score.innerText = scoreCount;
-
-        cardBody.classList.add("bg-success");
-        cardTitle.classList.remove("text-info");
-        cardTitle.classList.add("text-white");
-      }
-    } else {
-      {
-        console.log("Incorrect answer");
-        cardBody.classList.add("bg-danger");
-        cardTitle.classList.add("text-white");
-        cardTitle.classList.remove("text-info");
-      }
-    }
-    trueBtn.disabled = true;
-    falseBtn.disabled = true;
-  };
-
-  cardTitle.innerText = q.statement;
-
-  cardBody.appendChild(cardTitle);
-  cardFooter.appendChild(trueBtn);
-  cardFooter.appendChild(falseBtn);
-
-  card.appendChild(cardBody);
-  card.appendChild(cardFooter);
-  li.appendChild(card);
-
-  questionsList.appendChild(li);
 }
